a38ad0cedc38dff1943447bc259f7c697929dcbc
Pat <patemf2021@yahoo.com>
2024-12-10 16:40:00 -0500

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -10,4 +10,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+@RequestMapping("/employee")
+public class EmployeeService {
 
+	
+	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+	@ResponseBody
+	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+			throws JSONException {
+
+		JSONObject js = new JSONObject();
+		js.put("Name", "twtech");
+		js.put("Calling Name", "twtech");
+		js.put("DOB", "Dec-15-2024");
+		js.put("Game", "soccer , Basketball , Table Tenis");
+		js.put("continenets I have visited", "Asia, Middle-East, Africa, Europe, Asia, Oceania, South America, North America");
+    js.put("continent I am Yet to visit", "Antarctica");
+		return js.toString();
+}
+}
